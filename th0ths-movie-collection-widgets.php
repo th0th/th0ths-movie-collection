f3547087fdf170c436255cb1ec9250f19382edc9
th0th <th0th@returnfalse.net>
2011-11-30 01:41:51 +0200

    use correct css class and id format (convert the-name to the_name)

--- th0ths-movie-collection-widgets.php
+++ th0ths-movie-collection-widgets.php
@@ -37,10 +37,10 @@ class th0ths_Movie_Collection_Most_Recent extends WP_Widget {
             <?php if (count($movies) == 1) {
                 $movie = $movies[0];
                 ?>
-                <div class="th0ths-movie-collection-widget-poster">
+                <div class="th0ths_movie_collection_widget_poster">
                     <a href="<?php echo get_permalink($movie->id); ?>"><?php echo $movie->poster_html; ?></a>
                 </div>
-                <div class="th0ths-movie-collection-widget-title">
+                <div class="th0ths_movie_collection_widget_title">
                     <p>
                         <a href="<?php echo get_permalink($movie->id); ?>"><strong><?php echo $movie->title; ?></strong></a>
                     </p>
